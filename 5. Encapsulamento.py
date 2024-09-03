9de4acb3c23d743c0f31c88e262141e599bb3270
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-09-03 14:21:23 +0000

    .

---
+++ 5. Encapsulamento.py
@@ -0,0 +1,65 @@
+from abc import ABC, abstractmethod
+import os
+os.system("cls || clear")
+
+# Criando sua própria exceção
+class SaldoInsuficienteError(Exception):
+    pass
+
+class ValorNegativoError(Exception):
+    pass
+
+class Conta:
+    def __init__(self, numero_conta: int, agencia: int) -> None:
+        self.numero_conta = numero_conta
+        self.agencia = agencia
+        self._saldo = 0 # Atributo protegido
+
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    def sacar(self, valor) -> float:
+        # try - except
+        try:
+            self.__verificar_sacar(valor)
+        except SaldoInsuficienteError as error:
+            return f"Erro: {error}"
+        
+            self.saldo -= valor
+            return self._saldo
+        
+    def __verificar_sacar(self, valor): # Método privado.
+        if valor > self.saldo:
+            raise SaldoInsuficienteError("Saldo insuficiente.") # Lançando um erro
+        
+    def depositar(self, valor):
+        try:
+            self.__verificar_depositar(valor)
+        except ValorNegativoError as erro:
+            return f"Erro: {erro}"
+        
+        self._saldo += valor
+        return self._saldo
+    
+    def __verificar_depositar(self, valor):
+        if valor < 0:
+            raise ValorNegativoError("Não é possível depositar valor negativo.")
+        
+class ContaCorrente(Conta):
+    pass
+
+class ContaPoupanca(Conta):
+    pass
+
+# Instanciar classes
+conta_corrente = ContaCorrente(222, 333)
+conta_poupanca = ContaPoupanca(444, 555)
+
+print(conta_corrente.saldo)
+
+print(conta_corrente.sacar(200))
+
+print(conta_corrente.saldo)
+
+print(conta_corrente.depositar(-200))
